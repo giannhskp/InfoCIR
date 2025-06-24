00919a1a82047f286484209aa2dbb5ea6e26dad4
Giannis Kapetangeorgis <giannhskp168@gmail.com>
2025-06-24 14:21:19 +0300

    Fixed help pop-up styling

--- cir_app/src/callbacks/help_button.py
+++ cir_app/src/callbacks/help_button.py
@@ -1,11 +1,28 @@
-from dash import Output, callback, Input
+from dash import Output, callback, Input, State
+from dash import callback_context
 
 @callback(
     Output("help-popup", "is_open"),
-    Input("help-button", "n_clicks"),
+    [Input("help-button", "n_clicks"),
+     Input("close-help", "n_clicks")],
+    [State("help-popup", "is_open")],
     prevent_initial_call=True,
 )
-def help_button_is_pressed(_):
-    """Handle help button clicks"""
-    print('Help button is pressed')
-    return True 
+def toggle_help_popup(help_clicks, close_clicks, is_open):
+    """Handle help button clicks to toggle the popup"""
+    # Get the triggering component
+    ctx = callback_context
+    if not ctx.triggered:
+        return False
+    
+    # Get the button that was clicked
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    
+    # Simple logic: help button opens, close button closes
+    if button_id == "help-button":
+        return True
+    elif button_id == "close-help":
+        return False
+    
+    # Fallback: return current state
+    return is_open if is_open is not None else False 
